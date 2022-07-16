5640387d82139a13305801146a3455e23786deba
tsecher <me@thomas-secher.fr>
2022-07-16 16:16:53 +0200

    Fix relative generator

--- src/generator/installer/template/template.installer.js
+++ src/generator/installer/template/template.installer.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-const lang = require('<%= path =>/utils/commons/lang')('<%= id %>');
-const messenger = require('<%= path =>/utils/commons/messenger');
-const InstallerAbstract = require('<%= path =>/utils/installers/installer.abstract');
+const lang = require('<%= path %>/utils/commons/lang')('<%= id %>');
+const messenger = require('<%= path %>/utils/commons/messenger');
+const InstallerAbstract = require('<%= path %>/utils/installers/installer.abstract');
 
 class <%= class_name %> extends InstallerAbstract {
 
